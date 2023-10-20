9b36c1741c1bed10d0f29e09cec3d8a960d47168
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-20 12:25:47 +0900

    Update add_numbers.go

--- add_numbers.go
+++ add_numbers.go
@@ -1,8 +1,17 @@
 package main
-
 import "fmt"
 
 func main() {
     result := 1 + 1
     fmt.Printf("1 + 1 = %d\n", result)
+}
+
+#####################
+
+
+package main
+import "fmt"
+
+func main() {
+    fmt.Printf("Hello World!")
 }
