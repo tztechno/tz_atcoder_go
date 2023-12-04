23fec57e7483277380869a8955f6019c07b7efe8
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-04 14:37:21 +0900

    Update four_digits.go

--- four_digits.go
+++ four_digits.go
@@ -9,16 +9,11 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Enter a string: ")
 	S, _ := reader.ReadString('\n')
 	S = strings.TrimSpace(S)
-
 	n := len(S)
-
 	if n < 4 {
 		S = fmt.Sprintf("%0[4]*s", 4-n, "") + S
 	}
-
 	fmt.Println(S)
 }
