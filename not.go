1c75c1c440e4d6416fb0582d5c3356c59a537664
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-01 00:58:16 +0900

    Create not.go

---
+++ not.go
@@ -0,0 +1,20 @@
+//ABC178_A 
+//Not
+
+################################
+
+package main
+import (
+	"fmt"
+)
+func main() {
+	var N int
+	fmt.Scanf("%d %d", &N)
+	if N==1 {
+		fmt.Println(0)
+	} else {
+		fmt.Println(1)        
+    }
+}
+
+################################
