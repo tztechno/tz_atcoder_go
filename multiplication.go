a97e19b2ca235767aff3a003485aa0413033d229
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-20 17:53:46 +0900

    Create multiplication.go

---
+++ multiplication.go
@@ -0,0 +1,43 @@
+//abc169_a multiplication.go
+################################
+################################
+################################
+################################
+package main
+import (
+	"fmt"
+)
+func main() {
+  var i,j int
+  fmt.Scan(&i)
+  fmt.Scan(&j)
+  fmt.Println(i*j)
+}
+################################
+package main
+import "fmt"
+func main() {
+	var A, B int
+	fmt.Scan(&A, &B)
+	res := A * B
+	fmt.Println(res)
+}
+################################
+package main
+import (
+	"fmt"
+)
+func main() {
+	a,b:=0,0
+	fmt.Scanf("%d%d",&a,&b)
+	fmt.Println(a*b)
+}
+################################
+package main
+import "fmt"
+func main(){
+  var a,b int
+  fmt.Scan(&a,&b)
+  fmt.Print(a*b)
+}
+################################
