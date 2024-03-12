e02cef59f2556b16c9c8d08e380526b3c76792a6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-12 14:18:47 +0900

    Create abc091_a.go

---
+++ abc091_a.go
@@ -0,0 +1,86 @@
+//abc091_a.go
+################################
+var a,b,c int
+var A, B, C int32
+fmt.Scanf("%d %d %d", &a, &b, &c)
+fmt.Scan(&a,&b,&c)
+################################
+################################
+################################
+################################
+package main
+import "fmt"
+func main() {
+	var a, b, c int
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+	if a+b >= c {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+################################
+package main
+import "fmt"
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+	if c <= a+b {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
+################################
+package main
+import "fmt"
+func main () {
+    A, B, C := 0, 0, 0
+    fmt.Scanf("%d", &A)
+    fmt.Scanf("%d", &B)
+    fmt.Scanf("%d", &C)
+    if A + B >= C {
+        fmt.Println("Yes")
+    } else {
+        fmt.Println("No")
+    }
+}
+################################
+package main
+import (
+    "fmt"
+    )
+func main(){
+    var a,b,c int
+    fmt.Scan(&a,&b,&c)
+    if a+b>=c {
+        fmt.Println("Yes")
+    } else {
+        fmt.Println("No")
+    }
+}
+################################
+package main
+import "fmt"
+func main() {
+    var A, B, C int32
+    fmt.Scanf("%d%d%d", &A, &B, &C)
+    if A+B>=C {
+        fmt.Println("Yes")
+    } else {
+        fmt.Println("No")
+    }
+}
+################################
+package main
+import "fmt"
+func main() {
+    var A, B, C int
+    fmt.Scanf(&A, &B, &C)
+    if A+B>=C {
+        fmt.Println("Yes")
+    } else {
+        fmt.Println("No")
+    }
+}
+################################
